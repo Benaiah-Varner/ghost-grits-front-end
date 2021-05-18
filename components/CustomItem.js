c24ed5caa2feecc1b3f05ff1515d09bb91a920df
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2021-05-18 12:04:53 -0400

    cart fix

--- components/CustomItem.js
+++ components/CustomItem.js
@@ -18,12 +18,13 @@ const CustomItem = ({ item }) => {
   return (
     <div className="menu-item">
       <h4 onClick={deleteFromCart}>&times;</h4>
+      <p>wtf is going on</p>
       <div>
         <h3>{item.salad.name}</h3>
         <p>Dressing: {item.salad.choice_of_dressing}</p>
         <p>Toppings: {item.salad.choice_of_two_toppings}</p>
       </div>
-      <span>${item.salad.price}</span>
+      <h3>${item.salad.price}</h3>
     </div>
   );
 };
